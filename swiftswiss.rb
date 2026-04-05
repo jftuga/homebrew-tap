1eed05b915f493b41632d898ec0b2917260e8076
John Taylor <jftuga@users.noreply.github.com>
2026-04-05 08:50:52 -0400

    Update swiftswiss to 0.2.1

--- swiftswiss.rb
+++ swiftswiss.rb
@@ -1,15 +1,15 @@
 # typed: false
 # frozen_string_literal: true
 
-class SwiftSwiss < Formula
+class Swiftswiss < Formula
   desc "Swiss army knife CLI tool written in Swift using only native Apple frameworks"
   homepage "https://github.com/jftuga/swiftswiss"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jftuga/swiftswiss/releases/download/v0.2.0/swiftswiss_v0.2.0.tar.xz"
-      sha256 "04fa12f8c19db725849c7e3ca4427f8eb73d08709d36ca064ef8a8e1c9754251"
+      url "https://github.com/jftuga/swiftswiss/releases/download/v0.2.1/swiftswiss_v0.2.1.tar.xz"
+      sha256 "50f84fc76d8a7220b83797f082688083cb8ef845c6c3557e9fd03bdb30a496ac"
 
       define_method(:install) do
         bin.install "swiftswiss"
