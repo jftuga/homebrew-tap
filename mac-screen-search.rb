905f6012bb66a71e50786f350045073efd954585
John Taylor <jftuga@users.noreply.github.com>
2026-04-27 22:51:22 -0400

    Update mac-screen-search to 1.1.0

--- mac-screen-search.rb
+++ mac-screen-search.rb
@@ -4,12 +4,12 @@
 class MacScreenSearch < Formula
   desc "macOS CLI tool to capture screenshots, perform OCR, and highlight or redact matched text"
   homepage "https://github.com/jftuga/mac-screen-search"
-  version "1.0.1"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jftuga/mac-screen-search/releases/download/v1.0.1/mac-screen-search_v1.0.1.tar.xz"
-      sha256 "b09908700efa269f8a6f069cb1d689c7abf6c882fd2f2291dd2b820563173d00"
+      url "https://github.com/jftuga/mac-screen-search/releases/download/v1.1.0/mac-screen-search_v1.1.0.tar.xz"
+      sha256 "67a9abfcaedc912eda0feee83cc560f6d7aca744173ab39add959fb182ff83f6"
 
       define_method(:install) do
         bin.install "mac-screen-search"
