7d6145eb8729fcf1f501ba315148fed1dd4cf202
John Taylor <jftuga@users.noreply.github.com>
2026-05-10 14:11:14 -0400

    Update swiftswiss to 0.5.0

--- swiftswiss.rb
+++ swiftswiss.rb
@@ -4,12 +4,12 @@
 class Swiftswiss < Formula
   desc "Swiss army knife CLI tool written in Swift using only native Apple frameworks"
   homepage "https://github.com/jftuga/swiftswiss"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jftuga/swiftswiss/releases/download/v0.4.0/swiftswiss_v0.4.0.tar.xz"
-      sha256 "3831884dfab54a29c040a0ce0500255d855f047e33db9edd824a6b23c89e53c3"
+      url "https://github.com/jftuga/swiftswiss/releases/download/v0.5.0/swiftswiss_v0.5.0.tar.xz"
+      sha256 "97d4e9c6442729fceb65ba2c019203f276d7b90dee80972b38c979904ead7273"
 
       define_method(:install) do
         bin.install "swiftswiss"
